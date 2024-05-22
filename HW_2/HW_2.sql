5569208a94769d0b771cdd377ee244cfce16a544
KartashovIvan <vano.noname69@yandex.ru>
2024-05-22 20:15:34 +0300

    Отформатированы скрипыт второй домашки

--- HW_2/HW_2.sql
+++ HW_2/HW_2.sql
@@ -1,58 +1,38 @@
 --1. Посчитать количество заказов за все время
-select
-	COUNT(*)
-from
-	public.orders o;
+SELECT COUNT(*)
+FROM public.orders o;
 
 --2. Посчитать сумму по всем заказам за все время (учитывая скидки)
-select
-	CAST(SUM(unit_price * quantity * (1 - discount)) as numeric(9, 2)) as "Сумма всех заказов"
-from
-	order_details od;
+SELECT CAST(SUM(unit_price * quantity * (1 - discount)) AS numeric(9, 2)) AS "Сумма всех заказов"
+FROM order_details od;
 
 --3. Показать сколько сотрудников работает в каждом городе.quantity
-select
-	city as "Город",
-	COUNT(*) as "Колличество сотрудников"
-from
-	employees e
-group by
-	city ;
+SELECT city AS "Город",
+       COUNT(*) AS "Колличество сотрудников"
+FROM employees e
+GROUP BY city ;
 
 --4. Выявить самый продаваемый товар в штуках. Вывести имя продукта и его количество.
-select
-	p.product_name as "Имя продукта",
-	SUM(od.quantity) as "Количество товара шт."
-from
-	products p
-join order_details od on
-	p.product_id = od.product_id
-group by
-	p.product_name
-order by
-	"Количество товара шт." desc
-limit 1;
+SELECT p.product_name AS "Имя продукта",
+       SUM(od.quantity) AS "Количество товара шт."
+FROM products p
+JOIN order_details od ON p.product_id = od.product_id
+GROUP BY p.product_name
+ORDER BY "Количество товара шт." DESC
+LIMIT 1;
 
 --5. Выявить фио сотрудника, у которого сумма всех заказов самая маленькая
-select
-	e.last_name,
-	e.first_name
-from
-	employees e
-where
-	e.employee_id = (
-	select
-		employee_id
-	from (
-		select
-			employee_id,
-			sum(od.unit_price * od.quantity * (1 - od.discount)) as profit
-		from
-			orders o
-		join order_details od on
-			o.order_id = od.order_id
-		group by
-			employee_id
-		order by
-			profit asc
-		limit 1) as t1)
+SELECT e.last_name,
+       e.first_name
+FROM employees e
+WHERE e.employee_id =
+    (SELECT employee_id
+     FROM
+       (SELECT employee_id,
+               sum(od.unit_price * od.quantity * (1 - od.discount)) AS profit
+        FROM orders o
+        JOIN order_details od ON o.order_id = od.order_id
+        GROUP BY employee_id
+        ORDER BY profit ASC
+        LIMIT 1) AS t1);
+		
